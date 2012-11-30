e45d085a1c7bb7017dde862fb1c9ca1e6ce967d6
Kevin R <KRPent@gmail.com>
2012-11-30 16:32:11 -0500

    Dijkstra returns a dictionary of the best path and cost. The actual path is still a list of nodes.

--- graph.py
+++ graph.py
@@ -25,159 +25,154 @@ import os
 import json
 import random
 import graphviz
-import graphviz2
+from graphviz2 import Graphviz2
 
 
-class Graph:
-	m_name = ""
-	m_graph = {}
-	
-	m_dirWorking = "data/"
-	
-	m_gviz = None
+class DiGraph:
+    _directoryData  = "data/"
+    _painter        = None
+    
+    INFINITY = 10000
+    UNDEFINDED = None
+    
+    _name = "graph"
+    _data = {}
+    
 
-	def __init__(self, name):
-		self.m_name = name
-		
-		self.m_gviz = graphviz2.Graphviz2()
-		return
+    def __init__(self, name=None):
+        if name:
+            self._name = name
+        
+        self._painter = Graphviz2()
+        return
 
-	def __getitem__(self, index):
-		return self.m_graph[index]
+    def __getitem__(self, index):
+        return self._data[index]
 
-	def __iter__(self):
-		return self.m_graph.__iter__();
+    def __iter__(self):
+        return self._data.__iter__();
 
-	def keys(self):
-		return self.m_graph.keys();
+    def keys(self):
+        return self._data.keys();
 
-	def addNode(self, node):
-		if self.m_graph.has_key(node):
-			return False
+    def add_node(self, node):
+        if self._data.has_key(node):
+            return False
 
-		self.m_graph[node] = {}
-		return True
+        self._data[node] = {}
+        return True
 
-	def addLink(self, nodeFrom, nodeTo, cost=None):
-		if not cost:
-			cost = random.randrange(0, 5) + 1
-		
-		self.addNode(nodeFrom)
-		self.addNode(nodeTo)
-		
-		self.m_graph[nodeFrom][nodeTo] = cost
-		return
+    def add_edge(self, node_from, node_to, cost=None):
+        if not cost:
+            cost = random.randrange(1, 11)
+        
+        self.add_node(node_from)
+        self.add_node(node_to)
+        
+        self._data[node_from][node_to] = cost
+        return
 
-	def removeLink(self, nodeFrom, nodeTo, cost=None):
-		if not self.m_graph.has_key(nodeFrom) or not self.m_graph.has_key(nodeTo):
-			return -1
-		
-		if self.m_graph[nodeFrom].has_key(nodeTo):
-			if not cost:
-				cost = self.m_graph[nodeFrom][nodeTo]
-#				del self.m_graph[nodeFrom][nodeTo]
-				
-				if cost == 10000:
-					return -1
-				else:
-					self.m_graph[nodeFrom][nodeTo] = 10000
-					return cost
-			elif self.m_graph[nodeFrom][nodeTo] == cost:
-#				del self.m_graph[nodeFrom][nodeTo]
-				self.m_graph[nodeFrom][nodeTo] = 10000
-				
-				return cost
-			else:
-				return -1
-		else:
-			return -1
-	
-	def load(self, name=None):
-		if not name:
-			name = self.m_name
-		
-		pathJson = "%s%s.json" % (self.m_dirWorking, name)
-		if not os.path.exists(pathJson):
-			return False
-		
-		fhandle = open(pathJson, 'r')
-		self.m_graph = json.loads(fhandle.read())
-		fhandle.close()
-		
-		return
-	
-	def save(self):
-		if not os.path.exists(self.m_dirWorking):
-			os.mkdir(self.m_dirWorking)
-		
-		fhandle = open("%s%s.json" % (self.m_dirWorking, self.m_name), 'w')
-		fhandle.write(json.dumps(self.m_graph))
-		fhandle.close()
-		
-		return
-	
-	def export(self, source=None, sink=None):
-		grapher = graphviz.Graphviz()
-		grapher.generate(self.m_name, self.m_graph, source, sink)
-		
-		return
-	
-	def export2(self):
-		self.m_gviz.setGraph(self.m_graph)
-		self.m_gviz.generate(self.m_name)
-		
-		return
-	
-	def random(self, numNodes, numEdges, maxCost):
-		self.m_graph = {}
-		
-		for node in range(numNodes):
-			nameNode = "N%d" % node
-			self.addNode(nameNode)
-		
-		for edge in range(numEdges):
-			fromNode = random.choice(self.m_graph.keys())
-			
-			toNode = fromNode
-			while toNode == fromNode: # or self.m_graph[fromNode].has_key(toNode):
-				toNode = random.choice(self.m_graph.keys())
-			
-			cost = random.randrange(0, maxCost) + 1
-			self.addLink(fromNode, toNode, cost)
-		
-		return
-	
-	def setName(self, name=None):
-		if not name:
-			name = int(self.m_name) + 1
-		
-		self.m_name = name
-		
-		return
+    def remove_edge(self, node_from, node_to, cost=None):
+        if not self._data.has_key(node_from):
+            return -1
+        
+        if self._data[node_from].has_key(node_to):
+            if not cost:
+                cost = self._data[node_from][node_to]
+                
+                if cost == self.INFINITY:
+                    return -1
+                else:
+                    self._data[node_from][node_to] = self.INFINITY
+                    return cost
+            elif self._data[node_from][node_to] == cost:
+                self._data[node_from][node_to] = self.INFINITY
+                
+                return cost
+            else:
+                return -1
+        else:
+            return -1
+    
+    def load(self, name=None):
+        if not name:
+            name = self._name
+        
+        path_json = "%s%s.json" % (self._directoryData, name)
+        if not os.path.exists(path_json):
+            return False
+        
+        fhandle = open(path_json, 'r')
+        self._data = json.loads(fhandle.read())
+        fhandle.close()
+        
+        return True
+    
+    def save(self):
+        if not os.path.exists(self._directoryData):
+            os.mkdir(self._directoryData)
+        
+        fhandle = open("%s%s.json" % (self._directoryData, self._name), 'w')
+        fhandle.write(json.dumps(self._data))
+        fhandle.close()
+        
+        return
+    
+    def export(self, frames=False):
+        self._painter.setGraph(self._data)
+        self._painter.generate(self._name, frames)
+        
+        return
+    
+    def random(self, num_nodes, num_edges, max_cost):
+        self._data = {}
+        
+        for node in range(num_nodes):
+            self.add_node("N%d" % node)
+        
+        for edge in range(num_edges):
+            node_from = random.choice(self._data.keys())
+            
+            node_to = node_from
+            while node_to == node_from:
+                node_to = random.choice(self._data.keys())
+            
+            cost = random.randrange(0, max_cost) + 1
+            self.add_edge(node_from, node_to, cost)
+        
+        return
+    
+    def set_name(self, name):
+        if name:
+            self._name = name
+        
+            return True
+        else:
+            return False
 
 
 def main():
-	G = Graph("0")
-#	G.addLink("A", "B")
-#	G.addLink("A", "C")
-#	G.addLink("A", "D")
-#	
-#	G.addLink("B", "C")
-#	
-#	G.addLink("C", "D")
-#	G.addLink("C", "F")
-#	
-#	G.addLink("D", "F")
-#	
-#	G.addLink("E", "A")
-#	G.addLink("E", "B")
-#	
-#	G.save()
-	G.load()
-	G.export()
-	
-	return 0
+    G = Graph("0")
+#   G.add_edge("A", "B")
+#   G.add_edge("A", "C")
+#   G.add_edge("A", "D")
+#   
+#   G.add_edge("B", "C")
+#   
+#   G.add_edge("C", "D")
+#   G.add_edge("C", "F")
+#   
+#   G.add_edge("D", "F")
+#   
+#   G.add_edge("E", "A")
+#   G.add_edge("E", "B")
+#   
+#   G.save()
+    G.load()
+    G.export()
+    
+    return 0
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- graphviz2.py
+++ graphviz2.py
@@ -26,219 +26,219 @@ import codecs
 
 
 class Graphviz2:
-	m_dirWorking	= "data/gif/"
-	m_template		= "template.dot"
-	
-	m_formatBody	= None
-	m_formatNode	= "\"%s\" [color=\"%s\"]"
-	m_formatEdge	= "\"%s\" -> \"%s\" [label=\"%s\"%s]"
-	
-	m_formatRankSame	= "{ rank=same; %s }"
-	m_formatRankSource	= "\"%s\" [rank=\"source\"]"
-	m_formatRankSink	= "\"%s\" [rank=\"sink\"]"
-	
-	m_formatInfinity	= "#ef2929"
-	m_formatLegend		= "%-20s"
-	
-	m_infinity = 10000
-	
-	m_graph				= None
-	m_containerPaths	= [] # [{ path: "", colorNode: "", colorEdge:""}, {}]
-	m_containerRanks	= [] # [['A','B','C'],[]]
-	m_containerInfinity	= [] # ["", ""]
-	m_nodeSource		= None
-	m_nodeSink			= None
-	m_legendText		= [] # ["", ""]
-	m_legendColor		= "white"
-	
-	def __init__(self):
-		self.setup()
-		return
-	
-	def setup(self):
-		path = self.m_dirWorking + self.m_template
-		
-		if not os.path.exists(path):
-			return False
-		
-		fhandle = open(path, 'r')
-		self.m_formatBody = fhandle.read()
-		fhandle.close()
-		
-		return True
-	
-	def reset(self):
-		self.m_graph				= None
-		self.m_containerPaths		= []
-		self.m_containerRanks		= []
-		self.m_containerInfinity	= []
-		self.m_nodeSource			= None
-		self.m_nodeSink				= None
-		self.m_legendText			= []
-		
-		return
-	
-	def setGraph(self, graph):
-		self.m_graph = graph
-		
-		return
-	
-	def setRank(self, arrNodes):
-		self.m_containerRanks.append(arrNodes)
-		
-		return
-	
-	def setPath(self, strNodes, colorNode, colorEdge):
-		self.m_containerPaths.append({'path':strNodes, 'colorNode': colorNode, 'colorEdge': colorEdge})
-		
-		return
-	
-	def clearPath(self):
-		self.m_containerPaths = []
-		
-		return
-	
-	def setInfinity(self, node, toNode):
-		self.m_containerInfinity.append(node + toNode)
-		
-		return
-	
-	def clearInfinity(self):
-		self.m_containerInfinity = []
-		
-		return
-	
-	def setSourceSink(self, source, sink):
-		self.m_nodeSource = source
-		self.m_nodeSink = sink
-		
-		return
-	
-	def setLegendColor(self, color):
-		self.m_legendColor = color
-		
-		return
-	
-	def addLegendText(self, text):
-		self.m_legendText.append(text)
-		
-		return
-	
-	def clearLegendText(self):
-		self.m_legendText = []
-		
-		return
-	
-	def generate(self, name):
-		self.createDot(name)
-		self.createImage(name)
-		
-		return
-	
-	def createDot(self, name):
-		if not os.path.exists(self.m_dirWorking):
-			os.mkdir(self.m_dirWorking)
-		
-		fhandle = codecs.open("%s%s.dot" % (self.m_dirWorking, name), encoding='utf-8', mode='w')
-		
-		fields = self.parseGraph()
-		fields.append(self.parseRank())
-		fields.append(self.parseSourceSink())
-		fields.append(self.m_legendColor)
-		fields.extend(self.parseLegend())
-		
-		body = self.m_formatBody % tuple(fields)
-		fhandle.write(body)
-		
-		fhandle.close()
-		
-		return 
-	
-	def createImage(self, name, itype=None):
-		if not itype:
-			itype = "dot"
-		
-		pathDot = "%s%s.dot" % (self.m_dirWorking, name)
-		pathPng = "%s%s.png" % (self.m_dirWorking, name)
-		
-		if not os.path.exists(pathDot):
-			return False
-		
-		cmd = "%s %s -Tpng -o %s" % (itype, pathDot, pathPng)
-		if os.system(cmd) == 0:
-			return True
-		else:
-			return False
-	
-	def parseGraph(self):
-		nodes = ""
-		edges = ""
-		
-		for node,nitems in self.m_graph.iteritems():
-			nodes = nodes + self.m_formatNode % (node, self.findNodeColor(node)) + "\n\t"
-			
-			for toNode,cost in nitems.iteritems():
-				if (node + toNode) in self.m_containerInfinity:
-					temp = self.m_formatEdge % (node, toNode, u"∞", "style=dashed, color=\"%s\"" % self.m_formatInfinity)
-				else:
-					temp = self.m_formatEdge % (node, toNode, cost, self.findEdgeColor(node, toNode))
-				
-				if temp in edges:
-					continue
-				
-				edges += temp + "\n\t"
-		
-		return [nodes, edges]
-	
-	def parseRank(self):
-		rank = ""
-		for nodes in self.m_containerRanks:
-			rank += self.m_formatRankSame % ("\"%s\"" % "\" \"".join(nodes)) + "\n\t"
-		
-		return rank
-	
-	def parseSourceSink(self):
-		ss = ""
-		if self.m_nodeSource:
-			ss += self.m_formatRankSource % self.m_nodeSource + "\n\t"
-		
-		if self.m_nodeSink:
-			ss += self.m_formatRankSink % self.m_nodeSink + "\n"
-		
-		return ss
-	
-	def parseLegend(self):
-		legend = []
-		for i in range(0, 4):
-			if i < len(self.m_legendText):
-				legend.append(self.m_formatLegend % self.m_legendText[i])
-			else:
-				legend.append(self.m_formatLegend % "")
-		
-		return legend
-	
-	def findNodeColor(self, node):
-		color = ""
-		for pathInfo in self.m_containerPaths:
-			if node in pathInfo['path']:
-				color = pathInfo['colorNode']
-		
-		return color
-	
-	def findEdgeColor(self, node, toNode):
-		color = ""
-		edge = "%s%s" % (node, toNode)
-		for pathInfo in self.m_containerPaths:
-			if edge in pathInfo['path']:
-				color = ", penwidth=2, color=\"%s\"" % pathInfo['colorEdge']
-		
-		return color
+    m_dirWorking    = "data/gif/"
+    m_template      = "template.dot"
+    
+    m_formatBody    = None
+    m_formatNode    = "\"%s\" [color=\"%s\"]"
+    m_formatEdge    = "\"%s\" -> \"%s\" [label=\"%s\"%s]"
+    
+    m_formatRankSame    = "{ rank=same; %s }"
+    m_formatRankSource  = "\"%s\" [rank=\"source\"]"
+    m_formatRankSink    = "\"%s\" [rank=\"sink\"]"
+    
+    m_formatInfinity    = "#ef2929"
+    m_formatLegend      = "%-20s"
+    
+    m_infinity = 10000
+    
+    m_graph             = None
+    m_containerPaths    = [] # [{ path: "", colorNode: "", colorEdge:""}, {}]
+    m_containerRanks    = [] # [['A','B','C'],[]]
+    m_containerInfinity = [] # ["", ""]
+    m_nodeSource        = None
+    m_nodeSink          = None
+    m_legendText        = [] # ["", ""]
+    m_legendColor       = "white"
+    
+    def __init__(self):
+        self.setup()
+        return
+    
+    def setup(self):
+        path = self.m_dirWorking + self.m_template
+        
+        if not os.path.exists(path):
+            return False
+        
+        fhandle = open(path, 'r')
+        self.m_formatBody = fhandle.read()
+        fhandle.close()
+        
+        return True
+    
+    def reset(self):
+        self.m_graph                = None
+        self.m_containerPaths       = []
+        self.m_containerRanks       = []
+        self.m_containerInfinity    = []
+        self.m_nodeSource           = None
+        self.m_nodeSink             = None
+        self.m_legendText           = []
+        
+        return
+    
+    def setGraph(self, graph):
+        self.m_graph = graph
+        
+        return
+    
+    def setRank(self, arrNodes):
+        self.m_containerRanks.append(arrNodes)
+        
+        return
+    
+    def setPath(self, strNodes, colorNode, colorEdge):
+        self.m_containerPaths.append({'path':strNodes, 'colorNode': colorNode, 'colorEdge': colorEdge})
+        
+        return
+    
+    def clearPath(self):
+        self.m_containerPaths = []
+        
+        return
+    
+    def setInfinity(self, node, toNode):
+        self.m_containerInfinity.append(node + toNode)
+        
+        return
+    
+    def clearInfinity(self):
+        self.m_containerInfinity = []
+        
+        return
+    
+    def setSourceSink(self, source, sink):
+        self.m_nodeSource = source
+        self.m_nodeSink = sink
+        
+        return
+    
+    def setLegendColor(self, color):
+        self.m_legendColor = color
+        
+        return
+    
+    def addLegendText(self, text):
+        self.m_legendText.append(text)
+        
+        return
+    
+    def clearLegendText(self):
+        self.m_legendText = []
+        
+        return
+    
+    def generate(self, name):
+        self.createDot(name)
+        self.createImage(name)
+        
+        return
+    
+    def createDot(self, name):
+        if not os.path.exists(self.m_dirWorking):
+            os.mkdir(self.m_dirWorking)
+        
+        fhandle = codecs.open("%s%s.dot" % (self.m_dirWorking, name), encoding='utf-8', mode='w')
+        
+        fields = self.parseGraph()
+        fields.append(self.parseRank())
+        fields.append(self.parseSourceSink())
+        fields.append(self.m_legendColor)
+        fields.extend(self.parseLegend())
+        
+        body = self.m_formatBody % tuple(fields)
+        fhandle.write(body)
+        
+        fhandle.close()
+        
+        return 
+    
+    def createImage(self, name, itype=None):
+        if not itype:
+            itype = "dot"
+        
+        pathDot = "%s%s.dot" % (self.m_dirWorking, name)
+        pathPng = "%s%s.png" % (self.m_dirWorking, name)
+        
+        if not os.path.exists(pathDot):
+            return False
+        
+        cmd = "%s %s -Tpng -o %s" % (itype, pathDot, pathPng)
+        if os.system(cmd) == 0:
+            return True
+        else:
+            return False
+    
+    def parseGraph(self):
+        nodes = ""
+        edges = ""
+        
+        for node,nitems in self.m_graph.iteritems():
+            nodes = nodes + self.m_formatNode % (node, self.findNodeColor(node)) + "\n\t"
+            
+            for toNode,cost in nitems.iteritems():
+                if (node + toNode) in self.m_containerInfinity:
+                    temp = self.m_formatEdge % (node, toNode, u"∞", "style=dashed, color=\"%s\"" % self.m_formatInfinity)
+                else:
+                    temp = self.m_formatEdge % (node, toNode, cost, self.findEdgeColor(node, toNode))
+                
+                if temp in edges:
+                    continue
+                
+                edges += temp + "\n\t"
+        
+        return [nodes, edges]
+    
+    def parseRank(self):
+        rank = ""
+        for nodes in self.m_containerRanks:
+            rank += self.m_formatRankSame % ("\"%s\"" % "\" \"".join(nodes)) + "\n\t"
+        
+        return rank
+    
+    def parseSourceSink(self):
+        ss = ""
+        if self.m_nodeSource:
+            ss += self.m_formatRankSource % self.m_nodeSource + "\n\t"
+        
+        if self.m_nodeSink:
+            ss += self.m_formatRankSink % self.m_nodeSink + "\n"
+        
+        return ss
+    
+    def parseLegend(self):
+        legend = []
+        for i in range(0, 4):
+            if i < len(self.m_legendText):
+                legend.append(self.m_formatLegend % self.m_legendText[i])
+            else:
+                legend.append(self.m_formatLegend % "")
+        
+        return legend
+    
+    def findNodeColor(self, node):
+        color = ""
+        for pathInfo in self.m_containerPaths:
+            if node in pathInfo['path']:
+                color = pathInfo['colorNode']
+        
+        return color
+    
+    def findEdgeColor(self, node, toNode):
+        color = ""
+        edge = "%s%s" % (node, toNode)
+        for pathInfo in self.m_containerPaths:
+            if edge in pathInfo['path']:
+                color = ", penwidth=2, color=\"%s\"" % pathInfo['colorEdge']
+        
+        return color
 
 
 def main():
-	print "Usage: Use with the Graph class."
-	return 0
+    print "Usage: Use with the Graph class."
+    return 0
 
 
 if __name__ == '__main__':
-	main()
+    main()
