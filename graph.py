c91f43f2431a4e70cffdb4d4bc8eed761ad171e7
Kevin R <KRPent@gmail.com>
2012-12-01 00:16:47 -0500

    Fixed formatting for the DiGraph class.

--- graph.py
+++ graph.py
@@ -24,37 +24,86 @@
 import os
 import json
 import random
-import graphviz
-from graphviz2 import Graphviz2
+from graphviz import Graphviz
 
 
+## @brief Represents a directed graph of nodes and edges.
+# 
+# The directed graph is indentified with a supplied name and stored in the 
+# "data/json/" folder if saved. Initially the graph contains zero nodes, but
+# can be populated automatically by loading the data of the supplied name. The
+# graph can also be randomly generated, or manually configured.
 class DiGraph:
-    _directoryData  = "data/json/"
-    _painter        = None
+    ## The location that the graph data is stored as json objects.
+    _directory_data = "data/json/"
     
+    ## The Graphviz object that will be used to display the graph.
+    _painter = None
+    
+    ## An edge with this cost signifies that it has been removed from the graph.
+    # This value implies that any edge in the graph must be very small in 
+    # comparison.
     INFINITY = 10000
+    
+    ## Represents a NULL predecessor.
     UNDEFINDED = None
     
+    ## Specifies the identifier for the graph.
     _name = "graph"
+    
+    ## The dictionary of the graph. Each key represents a node and the value of
+    # the associated node is a dictionary of all the edges. The key of the edges
+    # dictionary is the node the edge terminates at and the value is the cost of
+    # the edge.
     _data = {}
     
-
+    ## Initializes the graph with an indentifier and Graphviz object.
+    #    
+    # @post The graph will contain the data specified by the identifier, if that
+    # data exist. If not, then the graph will be empty.
+    #
+    # @param self The object pointer.
+    # @param name The identifier for the graph by which the data is stored as or
+    # will be stored as.
+    # 
     def __init__(self, name=None):
         if name:
             self._name = name
         
-        self._painter = Graphviz2()
+        self.load()
+        
+        self._painter = Graphviz()
         return
 
-    def __getitem__(self, index):
-        return self._data[index]
+    ## Gets the edges of a specified node.
+    #
+    # @param self The object pointer.
+    # @param node The node whose edges are being queried.
+    # @retval {} A dictionary of the edges and thier cost if the node exist 
+    # within the graph or None if the node is not in the graph.
+    #
+    def __getitem__(self, node):
+        if self._data.has_key(node):
+            return self._data[node]
+        else:
+            return None
 
+    ## Iterator for the digraph object.
+    #
+    # @param self The object pointer.
+    # @retval iter An iterator that can be used to process each node of the 
+    # graph.
+    #
     def __iter__(self):
-        return self._data.__iter__();
+        return self._data.__iter__()
 
-    def keys(self):
-        return self._data.keys();
-
+    ## Adds a node to the graph.
+    #
+    # @param self The object pointer.
+    # @param node The name of the node that is to be added to the graph.
+    # @retval bool True if the node was added or False if the node already 
+    # existed in the graph.
+    #
     def add_node(self, node):
         if self._data.has_key(node):
             return False
@@ -62,6 +111,17 @@ class DiGraph:
         self._data[node] = {}
         return True
 
+    ## Adds a edge to the graph.
+    #
+    # @post The two nodes specified exist within the graph and their exist an
+    # edge between them of the specified value.
+    #
+    # @param self The object pointer.
+    # @param node_from The node that the edge starts at.
+    # @param node_to The node that the edge terminates at.
+    # @param cost The cost of the edge, if the cost is not specified a random
+    # cost is generated from 1 to 10.
+    #
     def add_edge(self, node_from, node_to, cost=None):
         if not cost:
             cost = random.randrange(1, 11)
@@ -72,6 +132,17 @@ class DiGraph:
         self._data[node_from][node_to] = cost
         return
 
+    ## Removes an edge from the graph.
+    #
+    # @param self The object pointer.
+    # @param node_from The node that the edge starts at.
+    # @param node_to The node that the edge terminates at.
+    # @param cost The cost of the edge, if the cost is not specified all edges
+    # between the nodes are removed.
+    # @retval int The cost of the edge that was removed. If the nodes of the 
+    # edge does not exist, or the cost of the edge was found to be infinity, or 
+    # if the specified edge does not exist, then -1 is returned.
+    #
     def remove_edge(self, node_from, node_to, cost=None):
         if not self._data.has_key(node_from):
             return -1
@@ -94,11 +165,19 @@ class DiGraph:
         else:
             return -1
     
-    def load(self, name=None):
-        if not name:
-            name = self._name
-        
-        path_json = "%s%s.json" % (self._directoryData, name)
+    ## Populates the graph with the data of the graph indentifier.
+    #
+    # @pre The _name variable has been set and there exist a ".json" file at
+    # the directory specified by _directory_data with the graph data.
+    # @post The _data dictionary will contain all the nodes and edges of the 
+    # graph.
+    #
+    # @param self The object pointer.
+    # @retval bool True if the graph was populated from the specified file, 
+    # False otherwise.
+    # 
+    def load(self):
+        path_json = "%s%s.json" % (self._directory_data, self._name)
         if not os.path.exists(path_json):
             return False
         
@@ -108,22 +187,55 @@ class DiGraph:
         
         return True
     
+    ## Stores the nodes and edges of the graph.
+    #
+    # @pre The _name variable has been set and the _data dictionary contains all
+    # the nodes and edges of the graph.
+    # @post There exist a ".json" file at the directory specified by 
+    # _directory_data with the graph data.
+    # 
+    # @param self The object pointer.
+    # 
     def save(self):
-        if not os.path.exists(self._directoryData):
-            os.mkdir(self._directoryData)
+        if not os.path.exists(self._directory_data):
+            os.mkdir(self._directory_data)
         
-        fhandle = open("%s%s.json" % (self._directoryData, self._name), 'w')
+        fhandle = open("%s%s.json" % (self._directory_data, self._name), 'w')
         fhandle.write(json.dumps(self._data))
         fhandle.close()
         
         return
     
-    def export(self, frames=False):
-        self._painter.setGraph(self._data)
-        self._painter.generate(self._name, frames)
+    ## Generates an image of the graph.
+    #
+    # @pre The _name variable has been set and the _data dictionary contains all
+    # the nodes and edges of the graph.
+    # @post There exist a ".png" file at the directory specified by Graphviz 
+    # with the graph image.
+    #
+    # @param frames Denotes whether the generated image will be a frame in a set
+    # of multiple images. 
+    # @param painter A new Graphviz object to paint the graph with.
+    # 
+    def export(self, frames=False, painter=None):
+        if not painter:
+            painter = self._painter
+        
+        painter.set_graph(self._data)
+        painter.generate(self._name, frames)
         
         return
     
+    ## Populates the graph with random data.
+    #
+    # @post The _data dictionary will contain all the nodes and edges of the 
+    # graph.
+    #
+    # @param self The object pointer.
+    # @param num_nodes The amount of nodes the graph should contain.
+    # @param num_edges The amount of edges the graph should contain.
+    # @param max_cost The maximum cost of any edge in the graph.
+    # 
     def random(self, num_nodes, num_edges, max_cost):
         self._data = {}
         
@@ -142,6 +254,15 @@ class DiGraph:
         
         return
     
+    ## Sets the graph indentifier.
+    #    
+    # @post The _name variable will contain the specified name.
+    #
+    # @param self The object pointer.
+    # @param name The identifier for the graph by which the data is stored as or
+    # will be stored as.
+    # @retval bool True is the name was set, False otherwise.
+    # 
     def set_name(self, name):
         if name:
             self._name = name
@@ -149,30 +270,14 @@ class DiGraph:
             return True
         else:
             return False
-
-
-def main():
-    G = Graph("0")
-#   G.add_edge("A", "B")
-#   G.add_edge("A", "C")
-#   G.add_edge("A", "D")
-#   
-#   G.add_edge("B", "C")
-#   
-#   G.add_edge("C", "D")
-#   G.add_edge("C", "F")
-#   
-#   G.add_edge("D", "F")
-#   
-#   G.add_edge("E", "A")
-#   G.add_edge("E", "B")
-#   
-#   G.save()
-    G.load()
-    G.export()
     
-    return 0
+    ## The reference to the Graphviz object.
+    #    
+    # @pre The _painter variable has been initialized with the Graphviz object.
+    #
+    # @param self The object pointer.
+    # @retval Graphviz The Graphviz object used by the graph.
+    #
+    def painter(self):
+        return self._painter
 
-
-if __name__ == '__main__':
-    main()
--- graphviz.py
+++ graphviz.py
@@ -22,87 +22,242 @@
 #  
 # 
 import os
+import codecs
 
 
+## @brief Generates images based on the Graph class.
+# 
+# Generates ".dot" markup files based on the directed graph and saves these 
+# files to in the "data/dot/" folder, then generates an image based on the file 
+# and stores the image in the "images/" folder.
+#
 class Graphviz:
-	m_dirWorking = "data/"
-	
-	m_header = "digraph network { rankdir=LR; \n\t%s \n\t%s \n}"
-	m_nodes = "\"%s\" [penwidth=1, shape=circle]"
-	m_edges = "\"%s\" -> \"%s\" [label=\"%d\", constraint=false]"
-	
-	m_rankSource = "\"%s\" [penwidth=1, shape=circle, rank=\"source\"]"
-	m_rankSink = "\"%s\" [penwidth=1, shape=circle rank=\"sink\"]"
-	
-	m_infinity = 10000
-	m_infinityEdge = "\"%s\" -> \"%s\" [label=\"~\", color=\"red\"]"
-	
-	def __init__(self):
-		return
-	
-	def generate(self, name, graph, source=None, sink=None):
-		self.createDot(name, graph, source, sink)
-		self.createImage(name)
-		
-		return
-	
-	def createDot(self, name, graph, source=None, sink=None):
-		if not os.path.exists(self.m_dirWorking):
-			os.mkdir(self.m_dirWorking)
-		
-		fhandle = open("%s%s.dot" % (self.m_dirWorking, name), 'w')
-		
-		items = self.parseGraph(graph, source, sink)
-		body = self.m_header % (items[0], items[1])
-		fhandle.write(body)
-		
-		fhandle.close()
-		
-		return 
-	
-	def createImage(self, name, itype=None):
-		if not itype:
-			itype = "circo"
-		
-		pathDot = "%s%s.dot" % (self.m_dirWorking, name)
-		pathPng = "%s%s.png" % (self.m_dirWorking, name)
-		
-		if not os.path.exists(pathDot):
-			return False
-		
-		cmd = "%s %s -Tpng -o %s" % (itype, pathDot, pathPng)
-		if os.system(cmd) == 0:
-			return True
-		else:
-			return False
-	
-	def parseGraph(self, graph, source=None, sink=None):
-		nodes = ""
-		edges = ""
-		
-		for node,nitems in graph.iteritems():
-			if node == source:
-				nodes = nodes + self.m_rankSource % node + "\n\t"
-			elif node == sink:
-				nodes = nodes + self.m_rankSink % node + "\n\t"
-			else:
-				nodes = nodes + self.m_nodes % node + "\n\t"
-			
-			for edge,cost in nitems.iteritems():
-				temp = self.m_edges % (node, edge, cost)
-				
-				if temp in edges:
-					continue
-				
-				edges = edges + temp + "\n\t"
-		
-		return [nodes, edges]
+    ## The location that the graph data is stored as dot markups.
+    _directory_data = "data/dot/"    
+    ## The location that the graph data is stored as images.
+    _directory_images = "images/"    
+    ## The location of the templates for the dot markup.
+    _directory_templates = ".templates/"
+    ## The name of the template for the dot markup.
+    _template_dot = "template.dot"
+    
+    ## The template for the entire dot markup.
+    _format_body = None
+    ## The template for each node in the dot markup.
+    _format_node = "\"%s\" [color=\"%s\"]"
+    ## The template for each edge in the dot markup.
+    _format_edge = "\"%s\" -> \"%s\" [label=\"%s\"%s]"
+    
+    ## The template for nodes of the same rank in the dot markup.
+    _format_rank same = "{ rank=same; %s }"
+    ## The template for the source node in the dot markup.
+    _format_rank_source = "\"%s\" [rank=\"source\"]"
+    ## The template for the sink node in the dot markup.
+    _format_rank_sink = "\"%s\" [rank=\"sink\"]"
+    
+    ## Color to display edges with infinity cost in the dot markup.
+    _format_infinity    = "#ef2929"
+    m_formatLegend      = "%-20s"
+    
+    m_infinity = 10000
+    
+    m_graph             = None
+    m_containerPaths    = [] # [{ path: "", colorNode: "", colorEdge:""}, {}]
+    m_containerRanks    = [] # [['A','B','C'],[]]
+    m_containerInfinity = [] # ["", ""]
+    m_nodeSource        = None
+    m_nodeSink          = None
+    m_legendText        = [] # ["", ""]
+    m_legendColor       = "white"
+    
+    def __init__(self):
+        self.setup()
+        return
+    
+    def setup(self):
+        path = self._directoryData + self.m_template
+        
+        if not os.path.exists(path):
+            return False
+        
+        fhandle = open(path, 'r')
+        self.m_formatBody = fhandle.read()
+        fhandle.close()
+        
+        return True
+    
+    def reset(self):
+        self.m_graph                = None
+        self.m_containerPaths       = []
+        self.m_containerRanks       = []
+        self.m_containerInfinity    = []
+        self.m_nodeSource           = None
+        self.m_nodeSink             = None
+        self.m_legendText           = []
+        
+        return
+    
+    def setGraph(self, graph):
+        self.m_graph = graph
+        
+        return
+    
+    def setRank(self, arrNodes):
+        self.m_containerRanks.append(arrNodes)
+        
+        return
+    
+    def setPath(self, strNodes, colorNode, colorEdge):
+        self.m_containerPaths.append({'path':strNodes, 'colorNode': colorNode, 'colorEdge': colorEdge})
+        
+        return
+    
+    def clearPath(self):
+        self.m_containerPaths = []
+        
+        return
+    
+    def setInfinity(self, node, toNode):
+        self.m_containerInfinity.append(node + toNode)
+        
+        return
+    
+    def clearInfinity(self):
+        self.m_containerInfinity = []
+        
+        return
+    
+    def setSourceSink(self, source, sink):
+        self.m_nodeSource = source
+        self.m_nodeSink = sink
+        
+        return
+    
+    def setLegendColor(self, color):
+        self.m_legendColor = color
+        
+        return
+    
+    def addLegendText(self, text):
+        self.m_legendText.append(text)
+        
+        return
+    
+    def clearLegendText(self):
+        self.m_legendText = []
+        
+        return
+    
+    def generate(self, name):
+        self.createDot(name)
+        self.createImage(name)
+        
+        return
+    
+    def createDot(self, name):
+        if not os.path.exists(self._directoryData):
+            os.mkdir(self._directoryData)
+        
+        fhandle = codecs.open("%s%s.dot" % (self._directoryData, name), encoding='utf-8', mode='w')
+        
+        fields = self.parseGraph()
+        fields.append(self.parseRank())
+        fields.append(self.parseSourceSink())
+        fields.append(self.m_legendColor)
+        fields.extend(self.parseLegend())
+        
+        body = self.m_formatBody % tuple(fields)
+        fhandle.write(body)
+        
+        fhandle.close()
+        
+        return 
+    
+    def createImage(self, name, itype=None):
+        if not itype:
+            itype = "dot"
+        
+        pathDot = "%s%s.dot" % (self._directoryData, name)
+        pathPng = "%s%s.png" % (self._directoryData, name)
+        
+        if not os.path.exists(pathDot):
+            return False
+        
+        cmd = "%s %s -Tpng -o %s" % (itype, pathDot, pathPng)
+        if os.system(cmd) == 0:
+            return True
+        else:
+            return False
+    
+    def parseGraph(self):
+        nodes = ""
+        edges = ""
+        
+        for node,nitems in self.m_graph.iteritems():
+            nodes = nodes + self.m_formatNode % (node, self.findNodeColor(node)) + "\n\t"
+            
+            for toNode,cost in nitems.iteritems():
+                if (node + toNode) in self.m_containerInfinity:
+                    temp = self.m_formatEdge % (node, toNode, u"∞", "style=dashed, color=\"%s\"" % self.m_formatInfinity)
+                else:
+                    temp = self.m_formatEdge % (node, toNode, cost, self.findEdgeColor(node, toNode))
+                
+                if temp in edges:
+                    continue
+                
+                edges += temp + "\n\t"
+        
+        return [nodes, edges]
+    
+    def parseRank(self):
+        rank = ""
+        for nodes in self.m_containerRanks:
+            rank += self.m_formatRankSame % ("\"%s\"" % "\" \"".join(nodes)) + "\n\t"
+        
+        return rank
+    
+    def parseSourceSink(self):
+        ss = ""
+        if self.m_nodeSource:
+            ss += self.m_formatRankSource % self.m_nodeSource + "\n\t"
+        
+        if self.m_nodeSink:
+            ss += self.m_formatRankSink % self.m_nodeSink + "\n"
+        
+        return ss
+    
+    def parseLegend(self):
+        legend = []
+        for i in range(0, 4):
+            if i < len(self.m_legendText):
+                legend.append(self.m_formatLegend % self.m_legendText[i])
+            else:
+                legend.append(self.m_formatLegend % "")
+        
+        return legend
+    
+    def findNodeColor(self, node):
+        color = ""
+        for pathInfo in self.m_containerPaths:
+            if node in pathInfo['path']:
+                color = pathInfo['colorNode']
+        
+        return color
+    
+    def findEdgeColor(self, node, toNode):
+        color = ""
+        edge = "%s%s" % (node, toNode)
+        for pathInfo in self.m_containerPaths:
+            if edge in pathInfo['path']:
+                color = ", penwidth=2, color=\"%s\"" % pathInfo['colorEdge']
+        
+        return color
 
 
 def main():
-	print "Usage: Use with the Graph class."
-	return 0
+    print "Usage: Use with the Graph class."
+    return 0
 
 
 if __name__ == '__main__':
-	main()
+    main()
